3c139f43cae8fbdcb6a778eb35c0b2936deb170c
Jorge Luis Silva Correa <jlsilva@uninorte.edu.co>
2017-02-16 18:13:59 -0500

    Primera lectura
    Primer archivo para la lectura de los datos.

---
+++ Procesamiento de señal/Emotiv/Lectura.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+"""            
+import time
+import matplotlib.pyplot as plt
+from emokit.emotiv import Emotiv
+
+if __name__ == "__main__":
+    n=0
+    f3=[]
+    fc5=[]
+    af3=[]
+    with Emotiv(display_output=True, verbose=True) as headset:
+        while True:
+            packet = headset.dequeue()
+            if packet is not None:
+                f3.append(packet.sensors['F3']['value'])
+                fc5.append(packet.sensors['FC5']['value'])
+                af3.append(packet.sensors['AF3']['value'])
+                plt.plot(f3,'b')
+                plt.plot(fc5,'r')
+                plt.plot(af3,'g')
+                n+=1
+                plt.pause(0.05)
+                pass
+            time.sleep(0.001)
+
+   
