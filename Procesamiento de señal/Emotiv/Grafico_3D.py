b6dd6e6da93556c638fe26de993156cfe2457193
Jorge Luis Silva Correa <jlsilva@uninorte.edu.co>
2017-02-23 10:06:11 -0500

    Espectograma

--- Procesamiento de señal/Emotiv/Grafico_3D.py
+++ Procesamiento de señal/Emotiv/Grafico_3D.py
@@ -7,38 +7,7 @@ Created on Thu Feb 16 22:08:35 2017
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
-<<<<<<< HEAD
-
-
-=======
 pi=np.pi
-"""
->>>>>>> origin/master
-fs = 10e3
-N = 1e5
-amp = 2 * np.sqrt(2)
-noise_power = 0.001 * fs / 2
-time = np.arange(N) / fs
-freq = np.linspace(1e3, 2e3, N)
-x = amp * np.sin(2*np.pi*freq*time)
-<<<<<<< HEAD
-x += np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
-
-"""
-t=100
-fs=160
-N=fs*t
-time = np.linspace(0, t, N)
-freq = 500
-amp=1
-x = amp * np.sin(2*np.pi*freq*time)
-"""
-#plt.plot(time,x)
-
-f, t, Sxx = signal.spectrogram(x, fs, window=('tukey',0.25))
-=======
-#x += np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
-"""
 ph=0
 amp=1
 freq=100
@@ -57,7 +26,6 @@ plt.show()
 #Tercera gráfica 
 plt.figure()
 f, t, Sxx = signal.spectrogram(y, fs,window=('tukey', 0.1))
->>>>>>> origin/master
 plt.pcolormesh(t, f, Sxx)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
