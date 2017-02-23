57c4fc02a686ab515fc0fef34e8f674ff21d8dfb
Jorge Luis Silva Correa <jlsilva@uninorte.edu.co>
2017-02-23 09:47:42 -0500

    Prueba

--- Procesamiento de señal/Emotiv/Grafico_3D.py
+++ Procesamiento de señal/Emotiv/Grafico_3D.py
@@ -7,29 +7,30 @@ Created on Thu Feb 16 22:08:35 2017
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
-pi=3.1316
+
 
 fs = 10e3
 N = 1e5
-amp = 20
-noise_power = 0.0000000001*fs/2
+amp = 2 * np.sqrt(2)
+noise_power = 0.001 * fs / 2
 time = np.arange(N) / fs
 freq = np.linspace(1e3, 2e3, N)
 x = amp * np.sin(2*np.pi*freq*time)
-#x += np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
-"""q
-dt=[]
-x=np.arange(-2*pi,2*pi,0.1)
-y=np.sin(x)
-for x in range(0,100):
-    dt.append(y)
-"""
-plt.plot(time,x)
-"""
-f, t, Sxx = signal.spectrogram(x, fs)
+x += np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
 
+"""
+t=100
+fs=160
+N=fs*t
+time = np.linspace(0, t, N)
+freq = 500
+amp=1
+x = amp * np.sin(2*np.pi*freq*time)
+"""
+#plt.plot(time,x)
+
+f, t, Sxx = signal.spectrogram(x, fs, window=('tukey',0.25))
 plt.pcolormesh(t, f, Sxx)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
 plt.show()
-"""
