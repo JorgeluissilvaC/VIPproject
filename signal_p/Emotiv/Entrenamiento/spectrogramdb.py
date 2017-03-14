f83b7c271ab70eef6a78953936143465dd702c58
Julián Sibaja <juliansibaja84@gmail.com>
2017-03-14 09:01:18 -0500

    [ADDED]spectrogram with database values

--- signal_p/Emotiv/Entrenamiento/spectrogramdb.py
+++ signal_p/Emotiv/Entrenamiento/spectrogramdb.py
@@ -2,36 +2,50 @@
 """
 Created on Thu Feb 16 22:08:35 2017
 
-@author: Karolay Ardila Salazar :v
+@author:Julián Sibaja García, 
+        Karolay Ardila Salazar,
+        Dayán Mendez Vasquez,
+        Jorge Silva Correa,
 """
 from scipy import signal
 from pylab import *
 import matplotlib.pyplot as plt
 import numpy as np
 import json
-import sqlite
-#with open('relajacion1.json', 'r') as fp:
-#  data = json.load(fp)
+import sqlite3
 
-conn = sqlite3.connect('database1.db') #connection object
+n = raw_input("Digite el numero de la prueba: ")
+conn = sqlite3.connect('database.db') #connection object
 c = conn.cursor()
 c.execute('''select * from '''+"s"+n)
-print("hi")
 data = c.fetchall()
+dic = {'AF3':[],'AF4':[],'F3':[],'F4':[],'F7':[],'F8':[],'FC5':[],'FC6':[],'T7':[],'T8':[],'P7':[],'P8':[],'O1':[],'O2':[]}
+
 for row in data:
-
-
+    dic['AF3'].append(row[2])
+    dic['AF4'].append(row[3])
+    dic['F3'].append(row[4])
+    dic['F4'].append(row[5])
+    dic['F7'].append(row[6])
+    dic['F8'].append(row[7])
+    dic['FC5'].append(row[8])
+    dic['FC6'].append(row[9])
+    dic['T7'].append(row[10])
+    dic['T8'].append(row[11])
+    dic['P7'].append(row[12])
+    dic['P8'].append(row[13])
+    dic['O1'].append(row[14])
+    dic['O2'].append(row[15])
+data = dic
 fs = 128.0
 ts = 1/fs
 time = np.arange(0,len(data['AF3']) * ts,ts)
 lst = ['AF3','AF4','F3','F4','F7','F8','FC5','FC6','T7','T8','P7','P8','O1','O2',]
-index = list(xrange(len(data['AF3'])))
+index = list(xrange(len(data)))
 
 k = 1
 while k == 1:
   for key in lst:
-    for i in index: 
-      data[key][i] = data[key][i][1]
 
     #Data vs time 
     plt.figure()
@@ -52,7 +66,8 @@ while k == 1:
   #plt.show()
     savefig(key + '.png', dpi = 300)
   k = 0
-
+conn.close()
+print "done"
 """
 #Example of a signal
 pi = np.pi
