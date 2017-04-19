73195aa0096323596742f67a7bdfc0e8dd2d7c91
Julián Sibaja <juliansibaja84@gmail.com>
2017-04-19 14:43:42 +0000

    sql_to_arff working with MySql[done]

--- signal_p/Emotiv/Entrenamiento/Sql_to_arff.py
+++ signal_p/Emotiv/Entrenamiento/Sql_to_arff.py
@@ -2,6 +2,7 @@
 import sqlite3
 import numpy as np
 import arff #Install arff con pip
+import mysql.connector
 """
 Tipos de prueba:
   r - relajación 
@@ -9,16 +10,32 @@ Tipos de prueba:
   mla - mover la mano izquierda
   mou - mover objeto hacia arriba
   mod - mover objeto hacia abajo
+  all - todas las ateriores
 """
 
-id_s='jorge' #Digite el sujeto 
+id_s='julian' #Digite el sujeto
 test_type='r' #Digite el tripo de prueba 
 
-conn = sqlite3.connect('database.db') #connection object
-c = conn.cursor()
-c.execute('''select n_trial,AF3,AF4,F3,F4,F7,F8,FC5,FC6,T7,T8,P7,P8 from '''+"s_"+id_s+" where (test_type = '"+test_type+"')")
-data = np.array(c.fetchall())
-conn.close()
+cnx = mysql.connector.connect(user =     'root',
+                                  password = 'uniatlantico',
+                                  host =     'vipdb.cd4eqkinbht7.us-west-2.rds.amazonaws.com',
+                                  database = 'vipdb')
+cursor = cnx.cursor()
+if test_type == 'all':
+    get_data = ("SELECT electrode,test_type,n_trial,F0,F08,F16,F24,F32,F4,F48,F56,F64,F72,F8,F88,F96,F104,F112,F12,F128"
+                " FROM c_" + id_s)
+else:
+    get_data = ("SELECT electrode,n_trial,F0,F08,F16,F24,F32,F4,F48,F56,F64,F72,F8,F88,F96,F104,F112,F12,F128"
+                " FROM c_" + id_s +" where (test_type = '"+test_type+"')")
+cursor.execute(get_data)
+data = np.array(cursor.fetchall())
+cursor.close()
+cnx.close()
+
+data_clms = ['electrode','test_type','n_trial',
+             'F0','F08','F16','F24','F32','F4',
+             'F48','F56','F64','F72','F8','F88',
+             'F96','F104','F112','F12','F128']
 
 name = id_s + '.arff'
-arff.dump(name, data, relation="whatever", names=['n_trial','AF3','AF4','F3','F4','F7','F8','FC5','FC6','T7','T8','P7','P8'])
+arff.dump(name, data, relation = "whatever", names = data_clms)
