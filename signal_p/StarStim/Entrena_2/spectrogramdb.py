0c140c16d220eb6992b33a25b0d1e8779f2b1a32
djmv <dayanm@uninorte.edu.co>
2017-06-11 17:28:06 -0500

    Aplicando convenciones de python

--- signal_p/StarStim/Entrena_2/spectrogramdb.py
+++ signal_p/StarStim/Entrena_2/spectrogramdb.py
@@ -16,34 +16,40 @@ Tipos de prueba:
 
 """
 from scipy import signal
-import numpy as np
 import scipy.io as sio
-import matplotlib.pylab as plt
+import numpy as np
+#import matplotlib.pylab as plt
 from sklearn.model_selection import cross_val_score
 from sklearn import preprocessing
-from sklearn import decomposition
+#from sklearn import decomposition
 from sklearn import svm
 
-def getDataFromDB(id_s):
+def get_data_db(id_s):
+    """importa los datos del dataset(.mat).
+    """
     mat_contents = sio.loadmat(id_s)
     conc = mat_contents['conc']
     rel = mat_contents['rel']
     conc = np.transpose(conc, (2, 1, 0))
     rel = np.transpose(rel, (2, 1, 0))
-    data_time = np.zeros((len(conc)*2,len(conc[0]),len(conc[0][0])))
+    data_time = np.zeros((len(conc)*2, len(conc[0]), len(conc[0][0])))
     data_time[0:len(conc)] = conc
-    data_time[len(conc)::] = rel          
+    data_time[len(conc)::] = rel
     return data_time
 
 
-def butter_filter(data, lowcut=3, highcut=25, fs=500, order=6): # Filter
-    nyq = 0.5*fs
+def butter_filter(data, highcut=25, fqc=500, order=6):
+    """Filtro pasabajas.
+    """
+    nyq = 0.5*fqc
     high = highcut/nyq
-    [b, a] = signal.butter(order, high, btype='low')
-    y = signal.lfilter(b, a, data)
-    return y
+    [b_c, a_c] = signal.butter(order, high, btype='low')
+    filt_sig = signal.lfilter(b_c, a_c, data)
+    return filt_sig
 
-def removeDC(data):
+def remove_dc(data):
+    """ Remueve el DC de una señal.
+    """
     ndata = np.zeros(np.shape(data))
     mean_v = np.mean(data, axis=2)
     for trial in range(0, len(data)):
@@ -53,59 +59,61 @@ def removeDC(data):
             ndata[trial][electrode] = v_trial # guardamos señal sin DC
     return ndata
 
-def downSampling(data, sc, Fs):
+def down_sampling(data, sc_v, fqc):
+    """Reduce la frecuencia de muestreo de una señal.
+    """
     if int(Fs % 2):
-        sub_signals = np.zeros((len(data), len(data[0]), len(data[0][0])/sc+1))
+        sub_signals = np.zeros((len(data), len(data[0]), len(data[0][0])/sc_v+1))
     else:
-        sub_signals = np.zeros((len(data), len(data[0]), len(data[0][0])/sc))
+        sub_signals = np.zeros((len(data), len(data[0]), len(data[0][0])/sc_v))
 
     for trial in range(0, len(data)):
         for electrode in range(0, len(data[trial])):
-            sub_signals[trial][electrode] = data[trial][electrode][::sc]
+            sub_signals[trial][electrode] = data[trial][electrode][::sc_v]
     return sub_signals
 
-def artifactRegression(data,reference):
+def artifact_regression(data, reference):
+    """Remueve los artifacts de una señal usando regresión. """
     reference = np.transpose(np.matrix(reference))
     data = np.transpose(np.matrix(data))
     op1 = (np.transpose(reference)*reference)/int(reference.shape[0]-1)
     op2 = (np.transpose(reference)*data)/int(data.shape[0]-1)
-    coeff,_,_,_=np.linalg.lstsq(op1,op2)
+    coeff, _, _, _ = np.linalg.lstsq(op1, op2)
     data = data - reference*coeff
     data = np.transpose(data)
     return data
 
-id_s = raw_input("[!] Digite el identificador del sujeto: ")
+S_ID = raw_input("[!] Digite el identificador del sujeto: ")
 
-datac = getDataFromDB(id_s)
-data = removeDC(datac)
-Y=butter_filter(data)
-scale= 10
-Fs = 500/scale # esto es porque fue submuestreado a 2
-sub_signals = downSampling(Y,int(scale),Fs)
+DATA = get_data_db(S_ID)
+D_DC = remove_dc(DATA) # Datos sin DC
+Y = butter_filter(D_DC)
+SCALE = 10
+FS = 500/SCALE # esto es porque fue submuestreado a 2
+SUB_SIGNAL = down_sampling(Y, int(SCALE), FS)
 
-ts = 1.0/Fs
-time = np.arange(0,len(data[0][0]) * ts,ts)
-f, t, S = signal.spectrogram(sub_signals, fs=Fs)
-ff = sub_signals.shape # Tamaño del array
-m_f = np.mean(S, axis = 3) # Potencia promedio para cada frecuencia
-feat = np.reshape(m_f, (ff[0]*ff[1],m_f.shape[2]))
-label = np.zeros((ff[0]*ff[1]))
-label[0:len(label)/2] = 1
-label[len(label)/2::] = 2
+TS = 1.0/FS
+TIME = np.arange(0, len(D_DC[0][0]) * TS, TS)
+F, T, S = signal.spectrogram(SUB_SIGNAL, fs=FS)
+ff = SUB_SIGNAL.shape # Tamaño del array
+M_F = np.mean(S, axis=3) # Potencia promedio para cada frecuencia
+FEATS = np.reshape(M_F, (ff[0]*ff[1], M_F.shape[2]))
+LABELS = np.zeros((ff[0]*ff[1]))
+LABELS[0:len(LABELS)/2] = 1
+LABELS[len(LABELS)/2::] = 2
 
 """
-Clasificación 
-"""      
+Clasificación
+"""
 min_max_scaler = preprocessing.MinMaxScaler()
-X_train_minmax = min_max_scaler.fit_transform(feat)
+X_train_minmax = min_max_scaler.fit_transform(FEATS)
 
 """
 pca = decomposition.PCA(n_components=len(feat))
 pca.fit(feat)
 V = pca.components_
 """
-clf = clf = svm.SVC(kernel='linear', C=1)
-scores = cross_val_score(clf, X_train_minmax, label, cv=10)
+clf = svm.SVC(kernel='linear', C=1)
+scores = cross_val_score(clf, X_train_minmax, LABELS, cv=10)
 scores.mean()
-print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
+print "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
